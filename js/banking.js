ee82363debadcbb07ad150f4283e0a73ea9f1025
Zaki003 <zakifaiazchy.work@gmail.com>
2021-08-17 00:08:17 +0600

    error handling done

--- js/banking.js
+++ js/banking.js
@@ -1,35 +1,99 @@
+// gets input and converts it to float, also clears input field
+function getInputValue(id) {
+    const inputValueText = document.getElementById(id).value;
+    const inputValue = parseFloat(inputValueText);
+    // clear input field 
+    document.getElementById(id).value = '';
+    return inputValue;
+}
+//  gets current value for display balance amount and converts to float 
+function getCurrentBalance(id) {
+    const previousValueText = document.getElementById(id).innerText;
+    const previousValue = parseFloat(previousValueText);
+    return previousValue;
+}
+// adding new amount with existing deposit and withdraw amount and updating it
+function setUpdatedValue(inputValue, totalId) {
+    const previousValueText = document.getElementById(totalId).innerText;
+    const previousValue = parseFloat(previousValueText);
+    const updatedValue = inputValue + previousValue;
+    document.getElementById(totalId).innerText = updatedValue;
+}
+
+
 // get deposit input from user and update total deposit
 document.getElementById('deposit-button').addEventListener('click', function () {
     // set deposit value to deposit total 
-    const depositValue = document.getElementById('deposit-input').value;
-    let currentDeposit = document.getElementById('deposit-total').innerText;
+    // const depositValue = document.getElementById('deposit-input').value;
+    const depositValue = getInputValue('deposit-input');
+    // const currentDeposit = getCurrentValue('deposit-total');
     // converting to number and adding new to existing deposit amount
-    currentDeposit = parseFloat(currentDeposit) + parseFloat(depositValue);
-    document.getElementById('deposit-total').innerText = currentDeposit;
+    /* const updatedDeposit = currentDeposit + depositValue;
+    document.getElementById('deposit-total').innerText = updatedDeposit; */
+    const existingBalance = getCurrentBalance('balance-total');
+    // update only if it's a positive value
+    if (depositValue > 0) {
+        setUpdatedValue(depositValue, 'deposit-total');
+        // update account balance
+        document.getElementById('balance-total').innerText = existingBalance + depositValue;
+    }
 
-    // update account balance
-    const existingBalance = parseFloat(document.getElementById('balance-total').innerText);
-    document.getElementById('balance-total').innerText = existingBalance + parseFloat(depositValue);
+    /* 
+    // validation of deposit value when input is empty 
+    if (document.getElementById('deposit-total').innerText == 'NaN') {
+        console.log('something is wrong');
+        document.getElementById('deposit-total').innerText = currentDeposit;
+    }
 
-    // empty deposit input field
-    document.getElementById('deposit-input').value = '';
 
+    // validation of balance value when deposit input is empty 
+    if (document.getElementById('balance-total').innerText == 'NaN') {
+        document.getElementById('balance-total').innerText = existingBalance;
+    } 
+    */
 });
+
+/* document.getElementById('deposit-input').addEventListener('keyup', function () {
+    const depositValue = document.getElementById('deposit-input').value;
+    // validation of code when no input is given 
+    if (depositValue != '') {
+        document.getElementById('deposit-button').removeAttribute('disabled');
+    }
+    else if (depositValue == '') {
+        document.getElementById('deposit-button').setAttribute('disabled', true);
+    }
+}) */
 
 // get withdraw input from user and update total withdraw
 document.getElementById('withdraw-button').addEventListener('click', function () {
     // set deposit value to deposit total 
-    const withdrawValue = document.getElementById('withdraw-input').value;
-    let currentWithdrawValue = document.getElementById('withdraw-total').innerText;
+    // const withdrawValue = document.getElementById('withdraw-input').value;
+    const withdrawValue = getInputValue('withdraw-input');
+    // const currentWithdrawValue = getCurrentValue('withdraw-total');
 
-    // converting to number and adding new to existing withdraw amount
-    currentWithdrawValue = parseFloat(currentWithdrawValue) + parseFloat(withdrawValue);
-    document.getElementById('withdraw-total').innerText = currentWithdrawValue;
+    //adding new to existing withdraw amount
+    /* const updatedWithdrawValue = currentWithdrawValue + withdrawValue;
+    document.getElementById('withdraw-total').innerText = updatedWithdrawValue; */
+    const existingBalance = getCurrentBalance('balance-total');
 
-    // update account balance 
-    const existingBalance = document.getElementById('balance-total').innerText;
-    document.getElementById('balance-total').innerText = parseFloat(existingBalance) - parseFloat(withdrawValue);
+    // updates only if it's a positive value
+    if (withdrawValue > 0 && withdrawValue <= existingBalance) {
+        setUpdatedValue(withdrawValue, 'withdraw-total');
+        // update account balance 
+        document.getElementById('balance-total').innerText = existingBalance - withdrawValue
+    }
 
-    // empty withdraw input field
-    document.getElementById('withdraw-input').value = '';
+
+    /* 
+    // validation of balance value when withdraw input is empty 
+    if (document.getElementById('withdraw-total').innerText == 'NaN') {
+        document.getElementById('withdraw-total').innerText = currentWithdrawValue;
+    }
+
+
+    // validation of balance value when withdraw input is empty 
+    if (document.getElementById('balance-total').innerText == 'NaN') {
+        document.getElementById('balance-total').innerText = existingBalance;
+    } 
+    */
 });
